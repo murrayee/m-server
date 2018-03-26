069109aba854ffdeba212d0b67639435bb9174f1
两瓶阿萨姆丶 <vivibearsir@163.com>
2018-03-26 17:59:37 +0800

    设计动态文章列表

---
+++ app/controller/topics.js
@@ -0,0 +1,40 @@
+/**
+ * Created by bear on 2018/3/2.
+ */
+'use strict';
+const Controller = require('egg').Controller;
+class TopicsController extends Controller {
+    async index() {
+        let result = await this.service.topics.index()
+        this.ctx.body = result;
+        this.status = 200;
+    }
+    async show() {
+        // let result=await this.service.users.index()
+        this.ctx.body = {
+            name: `${this.ctx.params.id} show`,
+        };
+        this.status = 200;
+    }
+    async create() {
+        // let result=await this.service.users.index()
+        this.ctx.body = `body: ${JSON.stringify(this.ctx.request.body)} create`;
+        this.status = 200;
+    }
+
+    async update() {
+        this.ctx.body = {
+            id: `${this.ctx.params.id} update`,
+        };
+        this.status = 200;
+    }
+
+    async destroy() {
+        this.ctx.body = {
+            id: `${this.ctx.params.id} destroy`,
+        };
+        this.status = 200;
+    }
+}
+module.exports = TopicsController;
+
--- app/service/topics.js
+++ app/service/topics.js
@@ -1,29 +1,13 @@
 /**
- * Created by bear on 2017/12/2.
+ * Created by bear on 2017/11/16.
  */
+const Service = require('egg').Service;
 
-
-module.exports = app => {
-    class TopicsService extends app.Service {
-        async index() {
-
-            let list=Array.from(new Array(100)).map((v,i)=>({
-                author:`我是你哥${i}`,
-                avatar: {type: String},
-                create_time: {type: Number},
-                title: {type: String},
-                abstract: {type: String},
-                relevant_img: {type: String},
-                praise_count: {type: Number},
-                review_count: {type: Number},
-                read_count: {type: Number},
-            }))
-
-            let topics = await this.ctx.model.topics.insert({
-
-            })
-            return topics
-        }
+class TopicsService extends Service {
+    async index(size = 10, num = 0) {
+        let result = await this.ctx.model.Topics.find({}).sort({'create_time': 1}).skip(size * num).limit(size)
+        return result
     }
-    return TopicsService
-};
+}
+
+module.exports = TopicsService;
