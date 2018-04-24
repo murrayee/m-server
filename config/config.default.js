98266a6c60ef262269da6d7672203ef621f63891
两瓶阿萨姆丶 <vivibearsir@163.com>
2018-04-24 11:12:37 +0800

    oauth2授权认证

--- config/config.default.js
+++ config/config.default.js
@@ -1,47 +1,56 @@
 'use strict';
 module.exports = {
-    keys: 'murray',
-    security: {
-        csrf: false,
+  keys: 'murray',
+  security: {
+    csrf: false,
+  },
+  middleware: [ 'errorHandler', 'uppercase' ],
+  errorHandler: {
+    match: '/api',
+  },
+  cors: {
+    origin: '*',
+    allowMethods: 'GET,HEAD,PUT,POST,DELETE,PATCH',
+  },
+  view: {
+    defaultViewEngine: 'nunjucks',
+    mapping: {
+      '.html': 'nunjucks',
     },
-    middleware: ['errorHandler','uppercase'],
-    errorHandler: {
-        match: '/api',
+  },
+  mongoose: {
+    url: 'mongodb://127.0.0.1/murray',
+    options: {
+      server: {
+        auto_reconnect: true,
+        poolSize: 10,
+      },
     },
-    cors: {
-        origin: '*',
-        allowMethods: 'GET,HEAD,PUT,POST,DELETE,PATCH'
+  },
+  io: {
+    init: {},
+    namespace: {
+      '/': {
+        connectionMiddleware: [ 'auth' ],
+        packetMiddleware: [ 'filter' ],
+      },
     },
-    view: {
-        defaultViewEngine: 'nunjucks',
-        mapping: {
-            '.html': 'nunjucks',
-        },
+  },
+  redis: {
+    client: {
+      port: 6379,
+      host: '127.0.0.1',
+      password: null,
+      db: 0,
     },
-    mongoose: {
-        url: 'mongodb://127.0.0.1/murray',
-        options: {
-            server: {
-                auto_reconnect: true,
-                poolSize: 10
-            }
-        }
-    },
-    io: {
-        init: {},
-        namespace: {
-            '/': {
-                connectionMiddleware: ['auth'],
-                packetMiddleware: ['filter'],
-            }
-        },
-    },
-    redis: {
-        client: {
-            port: 6379,
-            host: '127.0.0.1',
-            password: null,
-            db: 0,
-        },
-    },
+  },
+  oAuth2Server: {
+    debug: true,
+    grants: [
+      'password',
+      'authorization_code',
+      'refresh_token',
+      'client_credentials',
+    ],
+  },
 };
--- config/plugin.js
+++ config/plugin.js
@@ -1,23 +1,27 @@
 'use strict';
 module.exports = {
-    mongoose: {
-        enable: true,
-        package: 'egg-mongoose',
-    },
-    cors: {
-        enable: true,
-        package: 'egg-cors',
-    },
-    redis: {
-        enable: true,
-        package: 'egg-redis',
-    },
-    io: {
-        enable: true,
-        package: 'egg-socket.io',
-    },
-    nunjucks: {
-        enable: true,
-        package: 'egg-view-nunjucks'
-    },
-}
+  mongoose: {
+    enable: true,
+    package: 'egg-mongoose',
+  },
+  cors: {
+    enable: true,
+    package: 'egg-cors',
+  },
+  redis: {
+    enable: true,
+    package: 'egg-redis',
+  },
+  io: {
+    enable: true,
+    package: 'egg-socket.io',
+  },
+  nunjucks: {
+    enable: true,
+    package: 'egg-view-nunjucks',
+  },
+  oAuth2Server: {
+    enable: true,
+    package: 'egg-oauth2-server',
+  },
+};
